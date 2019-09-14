9977e3efa611f2872617abc43e47dfd518d5f6c4
Rohan Jain <code@rohanjain.in>
2019-09-14 11:42:31 +0530

    Cleanup reqwest error handling and categorisation

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
 use disturbance::config::Config;
-use disturbance::status::{categorise_response, Status};
-use reqwest;
+use disturbance::status::Status;
 use structopt::StructOpt;
-
-fn verify(config: &Config) -> Result<Status, Status> {
-    reqwest::get(&config.url)
-        .and_then(|mut resp| resp.text())
-        .map_err(|err| Status::ErrorResponse(err.to_string()))
-        .and_then(|text| categorise_response(text, config))
-}
 
 fn main() {
     let config = Config::from_args();
-    println!("Status: {:?}", verify(&config));
+    println!("Status: {:?}", Status::check(&config));
 }
