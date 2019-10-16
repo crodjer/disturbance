562c1965013b4d7819b66bc5bb1dedb5722c66af
Rohan Jain <crodjer@gmail.com>
2019-10-16 10:36:51 +0530

    Add workers which continually check status in parallel

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use disturbance::config::Config;
-use disturbance::status::Status;
+use disturbance::worker::workers;
 use structopt::StructOpt;
 
 fn main() {
     let config = Config::from_args();
-    println!("Status: {:?}", Status::check(&config));
+    workers(config);
 }
